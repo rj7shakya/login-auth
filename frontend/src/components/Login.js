0d7df51963b8923cacbe6041e2073150b2c26b10
rj7shakya <shakyarajad1@gmail.com>
2020-05-01 18:39:15 +0545

    form

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from "react";
+
+const Login = () => {
+  return <Fragment></Fragment>;
+};
+
+export default Login;
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+const Signup = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = user;
+
+  const onChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (name === "" || password === "" || email === "") {
+      console.log("please enter all fields ");
+    } else if (password !== password2) {
+      console.log("password do not match");
+    } else {
+      console.log(user);
+    }
+  };
+
+  return (
+    <div className="form">
+      <h1>Register a user</h1>
+      <form action="" onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" value={name} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" value={email} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            required
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Confirm Password</label>
+          <input
+            type="password"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+            required
+            minLength="6"
+          />
+        </div>
+        <input type="submit" value="signup" />
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
