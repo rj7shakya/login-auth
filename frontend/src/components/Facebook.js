d573ed0f712415ab8366c99f7ddc8cd6e97c85ac
rj7shakya <shakyarajad1@gmail.com>
2020-05-04 15:02:52 +0545

    fb login

---
+++ frontend/src/components/Facebook.js
@@ -0,0 +1,36 @@
+import React, { createContext } from "react";
+import AuthContext from "../context/authContext";
+import FacebookLogin from "react-facebook-login";
+
+const Facebook = () => {
+  const authContext = createContext(AuthContext);
+  const { googlelogin } = authContext;
+
+  const responseGoogle = (response) => {
+    console.log(response);
+    googlelogin({ idToken: response.tokenId });
+  };
+
+  return (
+    // <div className="form-container">
+    <GoogleLogin
+      clientId={`${process.env.REACT_APP_GOOGLE_CLIENT_ID}`}
+      render={(renderProps) => (
+        <button
+          onClick={renderProps.onClick}
+          disabled={renderProps.disabled}
+          className="btn btn-primary btn-block"
+        >
+          <i className="fa fa-google p2"></i>
+          Signup with google
+        </button>
+      )}
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy={"single_host_origin"}
+    />
+    // </div>
+  );
+};
+
+export default Facebook;
